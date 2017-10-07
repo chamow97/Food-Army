a6853ca8c2a0b2d90aaf4b0bd08852e8f2a85933
chamow <chamow97@hotmail.com>
2017-10-07 09:40:11 +0530

    Modify WSGI config file

--- sample_wsgi.py
+++ sample_wsgi.py
@@ -3,11 +3,11 @@ import sys
 
 path = '/home/foodarmy/food_management_system'
 
-if path not is sys.path:
+if path not in sys.path:
 	sys.path.append(path)
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'food_management_system.settings'
 
 from django.core.wsgi import get_wsgi_application
-from django.contrib.statifiles.handlers import StaticFilesHandler
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 application = StaticFilesHandler(get_wsgi_application())
